553023110fe3b7642f68157c84ade3c11a686e58
Issac Kelly <issac.kelly@gmail.com>
2013-10-22 10:16:18 -0700

    Fixes #1.

--- servee_image/models.py
+++ servee_image/models.py
@@ -8,8 +8,8 @@ class BaseImage(models.Model):
     Image model, You may notice that there's no image here,
     That's because you could still use this app, and subclass
     BaseImage, and do something interesting with your own
-    Image, like 
-    
+    Image, like
+
     """
     title = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
@@ -18,7 +18,7 @@ class BaseImage(models.Model):
 
     def __unicode__(self):
         return '%s' % self.title
-        
+
     def save(self, *args, **kwargs):
         self.modified = datetime.datetime.now()
         if not self.title:
@@ -26,13 +26,13 @@ class BaseImage(models.Model):
         if not self.uploaded:
             self.uploaded = datetime.datetime.now()
         super(BaseImage, self).save(*args, **kwargs) # Call the "real" save() method.
-        
+
     def get_absolute_url(self):
-        return '%s%s' % (settings.MEDIA_URL, self.image)
+        return self.image.url
 
     class Meta:
         abstract = True
-    
+
 class Image(BaseImage):
     """
     """
@@ -40,8 +40,8 @@ class Image(BaseImage):
         upload_to=IMAGE_UPLOAD_TO,
         storage=DEFAULT_STORAGE()
     )
-    
-    class Meta:        
+
+    class Meta:
         verbose_name = "servee image"
         verbose_name_plural = "servee images"
         ordering = ['modified',]
